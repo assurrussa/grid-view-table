44c3410acc812e1ed7447ce67ea02ab0681fff21
AssurrussA <assurrussa@inbox.ru>
2016-09-23 13:35:05 +0500

    changed view for editable

--- resources/views/column/editableClick.blade.php
+++ resources/views/column/editableClick.blade.php
@@ -86,7 +86,7 @@ if(is_array($value)) {
     echo (string)$callback;
 } ?>
 >
-    <?= $title; ?>
+    <?= $text; ?>
 </a>
 {{--
 <link href="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet"/>
