9db8cb67b66e71a6ecffa4de8296df83917d52ac
Artur Gafurov <assurrussa@yandex.ru>
2017-10-20 15:10:59 +0300

    Update Helpers.php

--- src/Helpers.php
+++ src/Helpers.php
@@ -17,18 +17,3 @@ if(!function_exists('amiGridColumnCeil')) {
         return \Assurrussa\GridView\GridView::columnCeil();
     }
 }
-if(!function_exists('isInt')) {
-    /**
-     * Проверяет содержит ли переменная целое значение.
-     * Возвращает true если типа integer или содержит только допустимые символы без учета начальных и конечных пробелов
-     * Не обрабатывает неопределенные значения! Для безопасной проверки элемента массива или свойства объекта используйте вместе с value().
-     *
-     * @param integer|string $val переменная
-     * @return boolean
-     */
-    function isInt($value)
-    {
-        // При преобразовании в int игнорируются все окружающие пробелы, но is_numeric допускает только пробелы в начале
-        return is_bool($value) ? false : filter_var($value, FILTER_VALIDATE_INT) !== false;
-    }
-}
